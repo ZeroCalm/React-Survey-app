299ad3fa2696d7a525f27837a0ae14824d886981
zeroCalm <smith.timothyh@gmail.com>
2017-05-22 09:25:07 -0700

    show input form

--- src/Usurvey.js
+++ src/Usurvey.js
@@ -14,10 +14,42 @@ var config = {
 
 
 class Usurvey extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			uid: uuid.v1(),
+			studentName: '',
+			answers: {
+				answer1: '',
+				answer2: '',
+				answer3: ''
+			},
+			isSubmitted: false
+
+		};
+	}
+
 	render(){
+		var studentName;
+		var questions;
+
+
+		if(this.state.studentName === '' && this.state.isSubmitted === false){
+				studentName = <div>
+				<h1> Hey Student, Please let us know your name: </h1>
+						<form>
+							<input type="text" placeholder="Enter your name" ref="name" />
+						</form>
+				</div>
+		}
+
 		return(
 			<div>
-			I am from Usurvey Component
+
+			{studentName}
+			--------------------------
+			{questions}
 
 			</div>
 		);
