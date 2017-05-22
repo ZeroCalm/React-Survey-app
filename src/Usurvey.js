3db539c4a9610712b5404c43e77eac5296c562ec
zeroCalm <smith.timothyh@gmail.com>
2017-05-22 07:58:21 -0700

    build Uservey component

---
+++ src/Usurvey.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Usurvey extends Component {
+	render(){
+		return(
+			<div>
+			I am from Usurvey Component
+
+			</div>
+		);
+	}
+}
+
+export default Usurvey;
