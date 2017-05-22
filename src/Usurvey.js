be1610d0dad1c58d34de6bae722d06aa129536f2
zeroCalm <smith.timothyh@gmail.com>
2017-05-22 09:11:52 -0700

    firebase script added

--- src/Usurvey.js
+++ src/Usurvey.js
@@ -1,4 +1,17 @@
 import React, { Component } from 'react'
+var firebase = require('firebase');
+var uuid = require('uuid');
+var config = {
+    apiKey: "AIzaSyCHHmpEjawOiDFLCQQ9EhVbH-nvsGL7IvY",
+    authDomain: "sample-survey-26f5a.firebaseapp.com",
+    databaseURL: "https://sample-survey-26f5a.firebaseio.com",
+    projectId: "sample-survey-26f5a",
+    storageBucket: "sample-survey-26f5a.appspot.com",
+    messagingSenderId: "162490067837"
+  };
+  firebase.initializeApp(config);
+
+
 
 class Usurvey extends Component {
 	render(){
